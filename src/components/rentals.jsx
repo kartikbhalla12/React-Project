ca8a13b23b9151544324ce49715c149b315bd846
dopebear23 <palashjain2312000@gmail.com>
2020-10-01 12:22:51 +0530

    Adding rentalService.js and changing rentals.jsx

--- src/components/rentals.jsx
+++ src/components/rentals.jsx
@@ -1,9 +1,41 @@
 import React, { Component } from 'react';
+import { getRentals } from './../services/rentalService';
 
 class Rentals extends Component {
-	render() {
-		return <h1>Rentals</h1>;
-	}
+  state = {
+    rentals: []
+  };
+  async componentDidMount() {
+    const { data: rentals } = await getRentals();
+    this.setState({ rentals });
+  }
+
+  render() {
+    return (
+      <table class='table'>
+        <thead>
+          <tr>
+            <th scope='col'>#</th>
+            <th scope='col'>Date Out</th>
+            <th scope='col'>Date Returned</th>
+            <th scope='col'>Movie Name</th>
+            <th scope='col'>Rental Fee</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.rentals.map((rental, i) => (
+            <tr>
+              <th scope='row'>{i + 1}</th>
+              <td>{rental.dateOut}</td>
+              <td>{rental.dateReturned}</td>
+              <td>{rental.movie.title}</td>
+              <td>{rental.rentalFee}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
 }
 
 export default Rentals;
