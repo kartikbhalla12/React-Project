6b41d9d1e4a763da01ab44470512d4ae7368f298
Suraj Singh <50519406+surajsly@users.noreply.github.com>
2020-10-19 23:23:13 +0530

    Login page UI fix

--- src/components/common/form.jsx
+++ src/components/common/form.jsx
@@ -1,98 +1,99 @@
-import React, { Component } from 'react';
-import Joi from 'joi-browser';
-import Input from './input';
-import Select from './select';
+import React, { Component } from "react";
+import Joi from "joi-browser";
+import Input from "./input";
+import Select from "./select";
 
 class Form extends Component {
-	state = {
-		data: {},
-		errors: {},
-	};
+  state = {
+    data: {},
+    errors: {},
+  };
 
-	validate = () => {
-		const errors = {};
-		const { data } = this.state;
-		const { error } = Joi.validate(data, this.schema, { abortEarly: false });
-		if (!error) return null;
+  validate = () => {
+    const errors = {};
+    const { data } = this.state;
+    const { error } = Joi.validate(data, this.schema, { abortEarly: false });
+    if (!error) return null;
 
-		for (let item of error.details) {
-			errors[item.path[0]] = item.message;
-		}
+    for (let item of error.details) {
+      errors[item.path[0]] = item.message;
+    }
 
-		return errors;
-	};
+    return errors;
+  };
 
-	validateProperty = (input) => {
-		// console.log(input);
-		const schema = { [input.name]: this.schema[input.name] };
-		const obj = { [input.name]: input.value };
-		const { error } = Joi.validate(obj, schema);
-		return error ? error.details[0].message : null;
-	};
+  validateProperty = (input) => {
+    // console.log(input);
+    const schema = { [input.name]: this.schema[input.name] };
+    const obj = { [input.name]: input.value };
+    const { error } = Joi.validate(obj, schema);
+    return error ? error.details[0].message : null;
+  };
 
-	handleSubmit = (event) => {
-		event.preventDefault();
-		const errors = this.validate();
-		// console.log(errors);
-		this.setState({ errors: errors || {} });
-		if (errors) return;
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const errors = this.validate();
+    // console.log(errors);
+    this.setState({ errors: errors || {} });
+    if (errors) return;
 
-		this.doSubmit();
-	};
+    this.doSubmit();
+  };
 
-	handleChange = ({ currentTarget: input }) => {
-		//de-structured  event and picked currentTarget and named it as input
-		const errors = { ...this.state.errors };
-		const errorMessage = this.validateProperty(input);
-		if (errorMessage) errors[input.name] = errorMessage;
-		else delete errors[input.name];
+  handleChange = ({ currentTarget: input }) => {
+    //de-structured  event and picked currentTarget and named it as input
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
 
-		this.setState({ errors });
+    this.setState({ errors });
 
-		const data = { ...this.state.data };
-		data[input.name] = input.value;
-		this.setState({ data });
-	};
+    const data = { ...this.state.data };
+    data[input.name] = input.value;
+    this.setState({ data });
+  };
 
-	renderButton = (label) => {
-		return (
-			<button
-				disabled={this.validate()}
-				type='submit'
-				className='btn btn-primary'>
-				{label}
-			</button>
-		);
-	};
+  renderButton = (label) => {
+    return (
+      <button
+        disabled={this.validate()}
+        type="submit"
+        className="btn btn-info btn-lg btn-block active"
+      >
+        {label}
+      </button>
+    );
+  };
 
-	renderInput = (name, label, type = 'text') => {
-		const { data, errors } = this.state;
-		return (
-			<Input
-				type={type}
-				name={name}
-				value={data[name]}
-				label={label}
-				onChange={this.handleChange}
-				error={errors[name]}
-			/>
-		);
-	};
+  renderInput = (name, label, type = "text") => {
+    const { data, errors } = this.state;
+    return (
+      <Input
+        type={type}
+        name={name}
+        value={data[name]}
+        label={label}
+        onChange={this.handleChange}
+        error={errors[name]}
+      />
+    );
+  };
 
-	renderSelect = (name, label, options) => {
-		const { data, errors } = this.state;
-		// console.log(this.state.data);
-		return (
-			<Select
-				name={name}
-				value={data[name]}
-				label={label}
-				onChange={this.handleChange}
-				error={errors[name]}
-				options={options}
-			/>
-		);
-	};
+  renderSelect = (name, label, options) => {
+    const { data, errors } = this.state;
+    // console.log(this.state.data);
+    return (
+      <Select
+        name={name}
+        value={data[name]}
+        label={label}
+        onChange={this.handleChange}
+        error={errors[name]}
+        options={options}
+      />
+    );
+  };
 }
 
 export default Form;
--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,47 +1,47 @@
-import React, { Component } from 'react';
-import Joi from 'joi-browser';
-import Form from './common/form';
-import { loginUser } from './../services/authService';
+import React, { Component } from "react";
+import Joi from "joi-browser";
+import Form from "./common/form";
+import { loginUser } from "./../services/authService";
 
 class LoginForm extends Form {
-	state = {
-		data: { username: '', password: '' },
-		errors: {},
-	};
-	schema = {
-		username: Joi.string().required().label('Username'),
-		password: Joi.string().required().label('Password'),
-	};
+  state = {
+    data: { username: "", password: "" },
+    errors: {},
+  };
+  schema = {
+    username: Joi.string().required().label("Username"),
+    password: Joi.string().required().label("Password"),
+  };
 
-	doSubmit = async () => {
-		const { username, password } = this.state.data;
-		try {
-			const { state } = this.props.location;
-			await loginUser(username, password);
-			window.location = state ? state.from.pathname : '/';
-		} catch (ex) {
-			if (ex.response && ex.response.status === 400) {
-				const errors = { ...this.state.errors };
-				errors.password = ex.response.data;
-				this.setState({ errors });
-			}
-		}
-	};
+  doSubmit = async () => {
+    const { username, password } = this.state.data;
+    try {
+      const { state } = this.props.location;
+      await loginUser(username, password);
+      window.location = state ? state.from.pathname : "/";
+    } catch (ex) {
+      if (ex.response && ex.response.status === 400) {
+        const errors = { ...this.state.errors };
+        errors.password = ex.response.data;
+        this.setState({ errors });
+      }
+    }
+  };
 
-	render() {
-		return (
-			<div className='container mt-5' style={{ maxWidth: '35vw' }}>
-				<h1 className='mb-5' style={{ textAlign: 'center' }}>
-					Login
-				</h1>
-				<form action='' onSubmit={this.handleSubmit}>
-					{this.renderInput('username', 'Username')}
-					{this.renderInput('password', 'Password', 'password')}
-					{this.renderButton('Login')}
-				</form>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="container mt-5" style={{ maxWidth: "35vw" }}>
+        <h1 className="mb-5 font-weight-bolder" style={{ textAlign: "center" }}>
+          Login
+        </h1>
+        <form action="" onSubmit={this.handleSubmit}>
+          {this.renderInput("username", "Username")}
+          {this.renderInput("password", "Password", "password")}
+          {this.renderButton("Login")}
+        </form>
+      </div>
+    );
+  }
 }
 
 export default LoginForm;
