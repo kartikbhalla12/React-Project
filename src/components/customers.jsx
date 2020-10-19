3b3913ced08d1d9681cf440bfaefc7ea893251b9
Suraj Singh <50519406+surajsly@users.noreply.github.com>
2020-10-19 23:36:37 +0530

    customer page fix and register page minor UI change

--- src/components/customers.jsx
+++ src/components/customers.jsx
@@ -1,9 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "../components/common/not-found.css";
 
 class Customers extends Component {
-	render() {
-		return <h1>Customers</h1>;
-	}
+  render() {
+    return (
+      <div id="notfound">
+        <div class="notfound">
+          <div class="notfound-404">
+            <h1>Working...</h1>
+          </div>
+          <h2>This page is in under construction </h2>
+          <p>
+            The page you are looking is under construction please visit later.
+          </p>
+          <a href="/">Go To Homepage</a>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Customers;
